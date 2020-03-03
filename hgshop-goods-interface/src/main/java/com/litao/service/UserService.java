9680794d873aa0f5cb926172908e14b5f9867282
asus <794119141@qq.com>
2020-03-03 18:47:08 +0800

    hgshop first commit

---
+++ hgshop-goods-interface/src/main/java/com/litao/service/UserService.java
@@ -0,0 +1,11 @@
+package com.litao.service;
+
+/**
+ * 
+ * @author zhuzg
+ *
+ */
+public interface UserService {
+
+	boolean login(String userName,String passWord);
+}
---
+++ hgshop-goods-service/src/main/java/com/litao/service/impl/UserServiceImpl.java
@@ -0,0 +1,18 @@
+package com.litao.service.impl;
+
+import org.apache.dubbo.config.annotation.Service;
+
+import com.litao.service.UserService;
+
+
+@Service(interfaceClass=UserService.class,version="1.0.0")
+public class UserServiceImpl implements UserService{
+
+	@Override
+	public boolean login(String userName, String passWord) {
+		// TODO Auto-generated method stub
+		return "admin".equals(userName) && "123456".equals(passWord);
+		
+	}
+
+}
